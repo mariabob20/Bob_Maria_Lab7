fd4dc2b6655fe0d3c16917bf7ca59700aff16dbf
Maria Bob <maria_bob16@yahoo.com>
2023-12-17 21:14:35 +0200

    Laborator

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bob_Maria_Lab7.Models
 {
     public class ShopList
     {
-         
-       [PrimaryKey, AutoIncrement]
+
+        [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
+
+
     }
 }
